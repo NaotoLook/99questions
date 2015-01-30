5d2f6971cd59b14f30f32a5cc3806eae41e0e771
増田直斗 <naoto.masuda619@gmail.com>
2015-01-30 22:07:08 +0900

    [add] Problem 11

--- 99.hs
+++ 99.hs
@@ -70,4 +70,13 @@ encode xs =
     in
       map (\xs -> (myLength xs,head xs)) list
     
+--Problem 11
+-- Modified run-length encoding.
+data ListType a = Single a | Multiple Int a
+                deriving (Show)
 
+encodeModified :: (Eq a) => [a] -> [ListType a] 
+encodeModified  = map encodeHelper . encode 
+    where
+      encodeHelper (1,x) = Single x
+      encodeHelper (n,x) = Multiple n x
